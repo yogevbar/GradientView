951641db875a2ab382496390f1d1fb0a4b961097
yogevbar <yogevbarber@gmail.com>
2016-10-02 02:31:09 +0300

    swift 3.0

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -10,32 +10,32 @@ import UIKit
 import GradientView
 
 final class ViewController: UIViewController {
-
-	// MARK: - Properties
-	
-	@IBOutlet var gradientView: GradientView!
-
-
-	// MARK: - UIViewController
-                            
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		
-		gradientView.colors = [
-			UIColor.whiteColor(),
-			UIColor(red: 0, green: 0, blue: 0.5, alpha: 1)
-		]
-		
-		// You can configure the locations as well
-//		gradientView.locations = [0.4, 0.6]
-	}
-
-
-	// MARK: - Actions
-	
-	@IBAction func showAlert(sender: UIButton) {
-		let alert = UIAlertController(title: "Dimming", message: "As part of iOS 7 design language, views should become desaturated when an alert view appears.", preferredStyle: .Alert)
-		alert.addAction(UIAlertAction(title: "Awesome", style: .Default, handler: nil))
-		presentViewController(alert, animated: true, completion: nil)
-	}
+    
+    // MARK: - Properties
+    
+    @IBOutlet var gradientView: GradientView!
+    
+    
+    // MARK: - UIViewController
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        gradientView.colors = [
+            UIColor.white,
+            UIColor(red: 0, green: 0, blue: 0.5, alpha: 1)
+        ]
+        
+        // You can configure the locations as well
+        //		gradientView.locations = [0.4, 0.6]
+    }
+    
+    
+    // MARK: - Actions
+    
+    @IBAction func showAlert(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Dimming", message: "As part of iOS 7 design language, views should become desaturated when an alert view appears.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Awesome", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
 }
